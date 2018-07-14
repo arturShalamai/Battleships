3be170b2f959a9b59793113058916cd85d765978
Shalamai Artur <arturshalamai@gmail.com>
2018-07-14 21:40:02 +0300

    Created Base Project. Added some DAL

---
+++ Battleships.DAL/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Battleships.DAL
+{
+    public class Game
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid FirstUserId { get; set; }
+
+        [Required]
+        public Guid SecondUserId { get; set; }
+
+        [Required]
+        public GameStatuses Status { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+        //If true = first, false = second
+        //I do this to make my objects cheaper
+        public bool Winner { get; set; }
+
+        public GameInfo GameInfo { get; set; }
+    }
+
+    public enum GameStatuses
+    {
+        Waiting,
+        Started,
+        Finished
+    }
+}
---
+++ Battleships.DAL/GameInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Battleships.DAL
+{
+    public class GameInfo
+    {
+        public Guid GameId { get; set; }
+
+        public string FirstUserField { get; set; }
+
+        public string SecondUserField { get; set; }
+        //To make my objects cheaper
+        //If true = first User, false = second
+        //Especially for in memory store
+        public bool Turn { get; set; }
+
+        public Game Game { get; set; }
+    }
+}
