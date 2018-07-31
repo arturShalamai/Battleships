48ce7b56f0f54eecb4976d5a1ae7231c16d6b9bb
Shalamai Artur <arturshalamai@gmail.com>
2018-07-31 22:43:38 +0300

    Changed Player Credentials

--- Battleships.Migrations/Migrations/20180731194125_ClientCredentialsInClient.Designer.cs
+++ Battleships.Migrations/Migrations/20180731194125_ClientCredentialsInClient.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Battleships.Migrations.Migrations
 {
     [DbContext(typeof(BattleshipsContext))]
-    partial class BattleshipsContextModelSnapshot : ModelSnapshot
+    [Migration("20180731194125_ClientCredentialsInClient")]
+    partial class ClientCredentialsInClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
